1f4beb81bc1451c07d320149e85e42373d26c9f3
kdk1026 <kdk1026@gmail.com>
2024-07-16 22:12:38 +0900

    마이페이지 수정

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,6 +1,18 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { getToken } from "../utils/token";
 
 function MyPage() {
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const accessToken = getToken();
+        if ( !accessToken ) {
+            navigate('/login');
+        }
+    }, [navigate]);
+
     const userInfo = useSelector((state) => state.user.userInfo);
 
     return (
